ad717d8c14b1efd6b5ac0b5c507cf8a4ac72fc6a
Alexandr Malygin <sambox@bk.ru>
2024-10-01 09:21:21 +0300

    edit

--- inc/main.h
+++ inc/main.h
@@ -1,10 +1,9 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <avr/io.h>
 #include <delay.h>
 #include "mega_adc.h"
 #define F_CPU 8000000UL
-
 
 #endif
--- inc/mega_adc.h
+++ inc/mega_adc.h
@@ -1,20 +1,23 @@
-//Необходимо добавить тип контроллера для настройки режима работы АЦП
-#ifndef MEGA_ADC_H
-#define MEGA_ADC_H
-//#define __AVR_ATmega128__
+// Необходимо добавить тип контроллера для настройки режима работы АЦП
+#ifndef _MEGA_ADC_H_
+#define _MEGA_ADC_H_
+// #define __AVR_ATmega128__
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-class MegaADC {
+class MegaADC
+{
 public:
-enum ADCReference {    
-    Aref = 0,                                //источник опорного напряжения подключеный к AREF
-    AVcc = (1 << REFS0),                    //источник опорного напряжения равный питанию контроллера
-    //Internal = (1 << REFS1),             //Резерв
-    InternalVCC = (1 << REFS0) | (1 << REFS1) //Внутренний источник опорного напряжения 2.56В
-};
+  enum ADCReference
+  {
+    Aref = 0,            // источник опорного напряжения подключеный к AREF
+    AVcc = (1 << REFS0), // источник опорного напряжения равный питанию контроллера
+    // Internal = (1 << REFS1),             //Резерв
+    InternalVCC = (1 << REFS0) | (1 << REFS1) // Внутренний источник опорного напряжения 2.56В
+  };
 
-enum ADCPrescaler {
+  enum ADCPrescaler
+  {
     Div2 = (1 << ADPS0),
     Div4 = (1 << ADPS1),
     Div8 = (1 << ADPS0) | (1 << ADPS1),
@@ -22,26 +25,24 @@ enum ADCPrescaler {
     Div32 = (1 << ADPS0) | (1 << ADPS2),
     Div64 = (1 << ADPS1) | (1 << ADPS2),
     Div128 = (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2)
-};
-    // Конструктор с инициализацией
-  MegaADC(uint8_t channel, ADCReference reference , ADCPrescaler prescaler );
+  };
+  // Конструктор с инициализацией
+  MegaADC(uint8_t channel, ADCReference reference, ADCPrescaler prescaler);
 
-
-   
-    void mega_adc_init(ADCReference reference, ADCPrescaler prescaler);
-    void mega_adc_start_conversion();
-    uint16_t mega_adc_read_result();
-    uint16_t mega_adc_auto_read(uint8_t channel);
-    uint16_t mega_adc_manual_read(uint8_t channel); 
-    void enable_adc_interrupts();
-    void disable_adc_interrupts();
-    void enable_adc_auto_mode();
-    void disable_adc_auto_mode();
-   static volatile uint16_t adcValue; // Для хранения значения АЦП
+  void mega_adc_init(ADCReference reference, ADCPrescaler prescaler);
+  void mega_adc_start_conversion();
+  uint16_t mega_adc_read_result();
+  uint16_t mega_adc_auto_read(uint8_t channel);
+  uint16_t mega_adc_manual_read(uint8_t channel);
+  void enable_adc_interrupts();
+  void disable_adc_interrupts();
+  void enable_adc_auto_mode();
+  void disable_adc_auto_mode();
+  static volatile uint16_t adcValue; // Для хранения значения АЦП
 
 private:
-    void mega_adc_select_channel(uint8_t channel);
-    // Номер канала ADC
+  void mega_adc_select_channel(uint8_t channel);
+  // Номер канала ADC
   uint8_t _channel;
   // Напряжение опорного сигнала
   ADCReference _reference;
